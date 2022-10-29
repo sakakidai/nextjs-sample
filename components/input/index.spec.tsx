caf97d8807da49cae5b7781ae8d96719a81e4210
sakakidai <sd.lybd1016@gmail.com>
2022-10-30 08:17:11 +0900

    テストの追加

--- components/input/index.spec.tsx
+++ components/input/index.spec.tsx
@@ -1,4 +1,4 @@
-import {render, screen, RenderResult} from '@testing-library/react'
+import {render, screen, RenderResult, fireEvent, getByRole} from '@testing-library/react'
 import { Input } from './index'
 
 describe('Input', () => {
@@ -18,7 +18,24 @@ describe('Input', () => {
 
   // テスト実行
   it('should empty in input on initial render', () => {
-    const inputNode = screen.getByLabelText('Username')
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+    expect(inputNode).toHaveValue('')
+  })
+
+  it('should show input text', () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+    fireEvent.change(inputNode, { target: {value: inputText} })
+    expect(inputNode).toHaveValue(inputText)
+  })
+
+  it('should reset when user clicks button', () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+    fireEvent.change(inputNode, { target: {value: inputText} })
+
+    const buttonNode = screen.getByRole('button', {name: 'Reset'}) as HTMLInputElement
+    fireEvent.click(buttonNode)
     expect(inputNode).toHaveValue('')
   })
 })
